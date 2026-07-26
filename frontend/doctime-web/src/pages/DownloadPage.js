8c01afcd2708947f7298656097ba54c41475f698
Arjun Nikam <your-email@gmail.com>
2026-07-26 15:04:56 +0530

    Update to Render URL, new APK link

--- frontend/doctime-web/src/pages/DownloadPage.js
+++ frontend/doctime-web/src/pages/DownloadPage.js
@@ -7,7 +7,7 @@ import AppleIcon from '@mui/icons-material/Apple';
 import MedicalServicesIcon from '@mui/icons-material/MedicalServices';
 import CheckCircleIcon from '@mui/icons-material/CheckCircle';
 
-const ANDROID_APK_LINK = 'https://drive.google.com/file/d/1A8UCGmn_bugB4F7Ly9KSg4YF1Q959T0q/view?usp=sharing';
+const ANDROID_APK_LINK = 'https://drive.google.com/file/d/1WAx-XritLJNAfUsVCn8XJYcyG_Ke8liV/view?usp=sharing';
 
 const features = [
     'Book doctor appointments instantly',
